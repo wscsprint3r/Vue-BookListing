3f39fcb8b2e510e41593043354079bd1e1601f7d
wscsprint3r <stefan.irimescu@beekeeper.io>
2019-02-06 14:03:09 +0100

    first step

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+        <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+        </ul>
     </div>
 </template>
 
@@ -10,7 +13,12 @@ export default {
     name: 'BookList',
     data() {
         return {
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
         }
     },
 }
@@ -20,5 +28,15 @@ export default {
 <<style lang="">
     h1, h2 {
         font-weight: normal
+    }
+
+    ul {
+    list-style-type: none;
+    padding: 0;
+    }
+
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>
